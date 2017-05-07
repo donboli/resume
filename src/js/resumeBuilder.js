14e19b1418008e0c52dd53a582f5fb1ae5dab19f
Wolfgang Becker <wolli.becker@gmail.com>
2017-05-07 19:12:00 +0200

    Cleanup resumeBuilder.js

--- src/js/resumeBuilder.js
+++ src/js/resumeBuilder.js
@@ -1,55 +1,3 @@
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span class="white-text role">%data%</span><hr>';
-
-var HTMLcontactGeneric = '<li class="flex-item"><span class="colored-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="colored-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="colored-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="colored-text">github</span><a href="#" target="_blank" class="white-text inline-link">%data%</a></li>';
-var HTMLlinkedin = '<li class="flex-item"><span class="colored-text">linkedIn</span><a href="#" target="_blank" class="white-text inline-link">%data%</a></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="colored-text">twitter</span><a href="#" target="_blank" class="white-text inline-link">%data%</a></li>';
-var HTMLlocation = '<li class="flex-item"><span class="colored-text">location</span><span class="white-text">%data%</span></li>';
-
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
-
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
-
-var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#" target="_blank">%data%';
-var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
-
-var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#" target="_blank">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
-
-var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#" target="_blank">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
-
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#" target="_blank">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<a href="#" target="_blank" class="certificate">%data%</a>';
-
-var internationalizeButton = '<button>Internationalize</button>';
-var googleMap = '<div id="map"></div>';
-
-var HTMLiconSkype = '<a><i class="fa fa-skype fa-2x" title="#" aria-hidden="true"></i></a>';
-var HTMLiconLinkedin = '<a href="#" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>';
-var HTMLiconGithub = '<a href="#" target="_blank"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>';
-
-var data = "%data%";
-
 window.bio = {
   name: "Wolfgang Becker",
   role: "Web Developer",
@@ -141,7 +89,6 @@ window.work = {
   }
 };
 
-
 window.projects = {
   projects: [{
     title: "Portfolio",
@@ -163,8 +110,6 @@ bio.display();
 projects.display();
 work.display();
 education.display();
-
-$("#mapDiv").append(googleMap);
 
 // Avoid flashes of unstyled content during page load
 $(window).load(function() {
